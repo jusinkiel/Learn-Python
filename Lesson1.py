439166614d5f42cd505cf144924d83c325b033d3
Justin <justinkielmontano@gmail.com>
2023-09-22 21:07:18 +1000

    Lesson 1

---
+++ Lesson1.py
@@ -0,0 +1,15 @@
+# inputs = input("Input a number : ")
+# print(type(inputs))
+
+# number = int(inputs)
+number2 = 10
+string1 = 'Malvin'
+
+# if number % 2 == 0:
+#     print('Even Number')
+# else:
+#     print('Odd Number')
+
+if number2 > 100:
+    print('Its big number')
+    print('Blablabla')
