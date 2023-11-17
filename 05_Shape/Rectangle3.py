8547752ee5a93cef2de43a33100ec17fbaf15793
Malvin Patrick Kurniawan <malvinpatrickk@gmail.com>
2023-11-17 18:53:44 +0700

    Add debugging

--- 05_Shape/Rectangle3.py
+++ 05_Shape/Rectangle3.py
@@ -3,8 +3,13 @@ length = int(input("Length Triangle: "))
 max_number = 1
 
 for row in range (0, length):  
+    # print("Change the line", row)
     for col in range (0, max_number):
         print('x', end=' ')
+        # print("Print the star", col)
 
-    max_number = max_number + 1
+    max_number = max_number + 1 # Python will increase every column & row
+    # print("Max Number", max_number)
+
+
     print ('')
