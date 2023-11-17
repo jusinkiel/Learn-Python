54a9810aaeb95881dc3dd00b87ade13a51ea47cf
Malvin Patrick Kurniawan <malvinpatrickk@gmail.com>
2023-11-17 19:01:39 +0700

    the other way to create triangle

---
+++ 05_Shape/rectangle4.py
@@ -0,0 +1,8 @@
+length = int(input("Length Triangle: "))  
+
+for row in range (0, length):  
+    for col in range (0, row + 1):
+        print('x', end=' ')
+
+
+    print ('')
---
+++ 05_Shape/rectangle5.py
@@ -0,0 +1,8 @@
+# tmp = 'x' * 3
+# print(tmp)     #xxx
+
+
+length = int(input("Length Triangle: "))  
+
+for row in range (0, length):  
+    print('x ' * (row + 1))
