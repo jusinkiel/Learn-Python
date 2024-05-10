1a3b4f3beb5b30d178160026a83ad1d76748d8a6
Justin <justinkielmontano@gmail.com>
2024-05-10 21:36:27 +1000

    update

--- 13_Food_Menu/Master_Menu.py
+++ 13_Food_Menu/Master_Menu.py
@@ -2,22 +2,43 @@
 # 1. Add new Menu -> saved into menu.txt
 # 2. Print saved menu
 
+def add_new_menu():
+    rda = open('food.txt', 'a')
+    name = input("Food Name : ")
+    price = input("What is price? : ")
+
+    print("{menu_name} - {menu_price}".format(
+        menu_name = name, 
+        menu_price = price), 
+        file=rda)
+    # rda.write(choose + "\n")
+    rda.close()
+
+def print_saved_menu():
+    with open('food.txt') as file:
+        for line in file.readlines():
+            print(line, end='')
+
 while True:
     print("Master Menu Options")
     print("1. Add New Menu")
     print("2. Print Saved Menu")
-    print("3. Exit")
+    print("3. Edit Menu")
+    print("4. Exit")
 
     menu = input("Choose option : ")
 
     if menu == '1':
-        rda = open('food.txt', 'a')
-        choose = input("Choose food : ")
-        print(choose, file= rda)
-        # rda.write(choose + "\n")
-        rda.close()
+        add_new_menu()
+        print_saved_menu()
+
+        with open('food.txt') as file:
+            for line in file.readlines():
+                print(line, end='')
     
     elif menu == '2':
+        print_saved_menu()
+
         with open('food.txt') as file:
             for line in file.readlines():
                 print(line, end='')
